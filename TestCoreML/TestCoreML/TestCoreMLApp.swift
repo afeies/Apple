d06e2db4f4dce1cc9ae2ad4b01d45ec0898b852c
afeies <alexfeies@live.com>
2025-06-23 20:13:15 -0400

    Add voice recognition for searching database

---
+++ TestCoreML/TestCoreML/TestCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCoreMLApp.swift
+//  TestCoreML
+//
+//  Created by Alex Feies on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct TestCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestCoreML/TestCoreMLTests/TestCoreMLTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestCoreMLTests.swift
+//  TestCoreMLTests
+//
+//  Created by Alex Feies on 6/22/25.
+//
+
+import Testing
+@testable import TestCoreML
+
+struct TestCoreMLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
