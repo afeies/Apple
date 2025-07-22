b0b629e23541e45e2b67b38f9cc16fea2ca239a4
afeies <alexfeies@live.com>
2025-07-21 20:14:15 -0400

    Initial macos timer app

---
+++ TimerApp/TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Alex Feies on 7/21/25.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimerApp/TimerAppTests/TimerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppTests.swift
+//  TimerAppTests
+//
+//  Created by Alex Feies on 7/21/25.
+//
+
+import Testing
+@testable import TimerApp
+
+struct TimerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
